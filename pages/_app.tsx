249fe0e2bd77b9eed5edb907f5cb0be5693aa9f1
Suzane Ferreira <suzane.ferreira@dtidigital.com.br>
2024-06-18 13:44:44 -0300

    fix: remove arquivo sem uso

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,9 @@ import type { AppProps } from 'next/app';
 import GlobalStyle from '../styles/globals';
 import { ThemeProvider } from '@mui/material';
 import theme from '../styles/theme';
-import Typography from '../styles/typography';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
-    <Typography />
     <GlobalStyle />
     <Component {...pageProps} />
   </ThemeProvider>
